efabecc6737d3efdaedeb45e1c9f659c2fd10a4c
yallu201 <dlruddlf07@gmail.com>
2021-05-25 21:59:50 +0900

    Convert component's return to jsx syntax without babel

--- simple1.js
+++ simple1.js
@@ -2,35 +2,21 @@
 function LikeButton() {
   const [liked, setLiked] = React.useState(false);
   const text = liked ? "좋아요 취소" : "좋아요";
-  return React.createElement(
-    "button",
-    { onClick: () => setLiked(!liked) },
-    text
-  );
+  return <button onClick={() => setLiked(!liked)}>{text}</button>;
 }
 
 function Container() {
   const [count, setCount] = React.useState(0);
-  return React.createElement(
-    "div",
-    null,
-    React.createElement(LikeButton),
-    React.createElement(
-      "div",
-      { style: { marginTop: 20 } },
-      React.createElement("span", null, "현재 카운트: "),
-      React.createElement("span", { style: { marginRight: 10 } }, count),
-      React.createElement(
-        "button",
-        { onClick: () => setCount(count + 1) },
-        "증가"
-      ),
-      React.createElement(
-        "button",
-        { onClick: () => setCount(count - 1) },
-        "감소"
-      )
-    )
+  return (
+    <div>
+      <LikeButton></LikeButton>
+      <div style={{ marginTop: 20 }}>
+        <span>현재 카운트:</span>
+        <span style={{ marginRight: 20 }}>{count}</span>
+        <button onClick={() => setCount(count + 1)}>증가</button>
+        <button onClick={() => setCount(count - 1)}>감소</button>
+      </div>
+    </div>
   );
 }
 const domContainer = document.getElementById("root");
